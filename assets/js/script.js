0623205971156701cbfe1b6ac4cbc9471bdb4423
adalpo87 <adalpo87@gmail.com>
2021-04-13 17:10:49 +0200

    jsnack1 completato

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+/* **JSnack 1**
+l software deve chiedere per 5 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+Esegui questo programma in due versioni, con il for e con il while. */
+
+
+/* var i = 1;
+var somma = 0; 
+
+while(i < 6){
+ var n = parseInt(prompt("Inserisci un numero"));
+ console.log(n);
+ somma += n;
+ i++;
+ document.getElementById('lista').innerHTML += n;
+}
+console.log(somma);
+
+
+for (i=1; i<6; i++){
+  var n = parseInt(prompt("Inserisci un numero"));
+  document.getElementById('lista').innerHTML += n;
+  somma += n;
+  console.log(n);
+}
+
+console.log(somma); */
+
